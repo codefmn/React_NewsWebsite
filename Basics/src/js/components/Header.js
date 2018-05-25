415fed3a2af4862e84acda27cdbbc6af6d6f07b0
mfxy <mfxyzm@outlook.com>
2018-05-25 11:12:20 +1000

    Add Header Component

---
+++ Basics/src/js/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Header extends React.Component{
+    render(){
+        return(
+            <header>
+                <h1>This is Header.</h1>
+            </header>
+        )
+    }
+}
--- Basics/src/js/index.js
+++ Basics/src/js/index.js
@@ -1,7 +1,20 @@
 var React = require('react');
 var ReactDom = require('react-dom');
+import Header from './components/Header';
 
+class Index extends React.Component{
+    render(){
+        return(
+            //one node div
+            <div id="wrapper">
+                <Header/>
+            </div>
+        );
+    }
+}
+
+//entry
 ReactDom.render(
-    <h1>Hello</h1>,
+    <Index/>,
     document.getElementById('container')
 );
