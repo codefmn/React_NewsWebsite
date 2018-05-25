91c6b3986f55574cc0e7afc1062f9d1f58f09531
mfxy <mfxyzm@outlook.com>
2018-05-25 11:38:26 +1000

    Update index.js

--- Basics/src/js/index.js
+++ Basics/src/js/index.js
@@ -1,13 +1,18 @@
 var React = require('react');
 var ReactDom = require('react-dom');
 import Header from './components/Header';
+import Body from './components/Body';
+import Footer from './components/Footer';
 
 class Index extends React.Component{
     render(){
+        var header = <Header/>;
         return(
             //one node div
-            <div id="wrapper">
-                <Header/>
+            <div>
+                {header}
+                <Body/>
+                <Footer/>
             </div>
         );
     }
