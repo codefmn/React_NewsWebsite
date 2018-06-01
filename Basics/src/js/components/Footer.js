c17e8c0d473d99543b8fef1bbe49094763f6e860
mfxy <mfxyzm@outlook.com>
2018-06-01 18:50:02 +1000

    Add footer css module

--- Basics/src/js/components/Footer.js
+++ Basics/src/js/components/Footer.js
@@ -1,9 +1,11 @@
 import React from 'react';
 
+var footerStyle = require("../../css/footer.css")
+
 export default class Footer extends React.Component{
     render(){
         return(
-            <footer>
+            <footer className={footerStyle.footerColor}>
                 <h2>This is Footer.</h2>
             </footer>
         );
