7d773823eda1e26c5aea4c80964812a8db01ccb9
mfxy <mfxyzm@outlook.com>
2018-06-01 20:35:36 +1000

    Add List and Details Component

---
+++ Basics/src/js/components/Details.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class Details extends React.Component{
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>Details 1 in Index </li>
+                    <li>Details 2 in Index</li>
+                </ul>
+            </div>
+        );
+    }
+}
---
+++ Basics/src/js/components/List.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class List extends React.Component{
+    render(){
+        return(
+            <div>
+                <ul>
+                    <li>list1</li>
+                    <li>list2</li>
+                </ul>
+            </div>
+        );
+    }
+}
