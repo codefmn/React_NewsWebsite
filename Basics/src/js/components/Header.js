e7bf9ab07e4fef6baa3c1d3ce0acbe2a2c454d4b
mfxy <mfxyzm@outlook.com>
2018-06-01 20:57:08 +1000

    Add Link for router

--- Basics/src/js/components/Header.js
+++ Basics/src/js/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {Link} from 'react-router';
 
 export default class Header extends React.Component{
     constructor(){
@@ -27,6 +28,11 @@ export default class Header extends React.Component{
 
         return(
             <header>
+                <ul>
+                    <li><Link to={'/'}>Home</Link></li>
+                    <li><Link to={'/details'}>Details in Index</Link></li>
+                    <li><Link to={'/list/123'}>List Page</Link></li>
+                </ul>
                 <h1 style={style.h1}>This is Header.(const style)</h1>
                 <h2 className="fontColor">Header 2 (className)</h2>
                 <h3 style={style.h3} onClick={this.switchH3.bind(this)}>Header 3 (switch on click)</h3>
