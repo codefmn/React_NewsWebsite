9f0da336581d7ac576690545cfdd83e215f26e93
mfxy <mfxyzm@outlook.com>
2018-06-03 18:55:47 +1000

    Install react-responsive

--- src/js/root.js
+++ src/js/root.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router,Route,hashHistory} from 'react-router';
+import MediaQuery from 'react-responsive';
 import PCIndex from './components/PCIndex';
+import MobileIndex from './components/MobileIndex';
 import 'antd/dist/antd.css';
 
 export default class Root extends React.Component{
     render(){
         return(
             <div>
-                <PCIndex />
+                <MediaQuery query="(min-device-width: 1224px)">
+                    <PCIndex />
+                </MediaQuery>
+                <MediaQuery query="(max-device-width: 1224px)">
+                    <MobileIndex />
+                </MediaQuery>
             </div>
         );
     }
