c33ba5706f3ae0333ba9ab274139dbeedbe2a824
mfxy <mfxyzm@outlook.com>
2018-06-04 16:54:43 +1000

    Add Footer Component

--- src/js/components/MobileIndex.js
+++ src/js/components/MobileIndex.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import MobileHeader from './MobileHeader';
-
+import Footer from './Footer';
 
 export default class MobileIndex extends React.Component{
     render(){
         return(
             <div>
                 <MobileHeader />
+                <Footer />
             </div>
         );
     }
--- src/js/components/PCIndex.js
+++ src/js/components/PCIndex.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import PCHeader from './PCHeader';
-
+import Footer from './Footer';
 
 export default class PCIndex extends React.Component{
     render(){
         return(
             <div>
                 <PCHeader />
+                <Footer />
             </div>
         );
     }
