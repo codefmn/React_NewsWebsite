de2f54b7304bc92bea175f7f99c6ecf6ad04c1e7
mfxy <mfxyzm@outlook.com>
2018-06-04 16:04:04 +1000

    Update MobileHeader

--- src/js/components/MobileHeader.js
+++ src/js/components/MobileHeader.js
@@ -1,19 +1,14 @@
 import React from 'react';
-import {Row, Col} from 'antd';
 
 export default class MobileHeader extends React.Component{
     render(){
         return(
-            <header id="mobileHeader">
-                <Row>
-                    <Col span={24}>
-                        <a href="/" className="mobileLogo">
-                            <img src="./src/images/icon.png" alt="logo"/>
-                            <span>News Website</span>
-                        </a>
-                    </Col>
-                </Row>
-            </header>
+        <header className="mobileLogo">
+            <a href="/">
+                <img src="./src/images/icon.png" alt="logo"/>
+                <span>News Website</span>
+            </a>
+        </header>
         )
     }
 }
