228b3241e913b4405967e00757c0057fa1f72ae3
mfxy <mfxyzm@outlook.com>
2018-06-24 11:02:34 +1000

    Add Tabs for NewsList

--- src/js/components/PCIndex.js
+++ src/js/components/PCIndex.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import PCHeader from './PCHeader';
 import Footer from './Footer';
+import PCNewsList from './PCNewsList';
+import {Tabs} from 'antd';
 
 export default class PCIndex extends React.Component{
     render(){
         return(
             <div>
                 <PCHeader />
+                <Tabs>
+                    <Tabs.TabPane key="1" tab="World">
+                        <PCNewsList type="guoji" count={10}/>
+                    </Tabs.TabPane>
+                    <Tabs.TabPane key="2" tab="Technology">
+                        <PCNewsList type="keji" count={10}/>
+                    </Tabs.TabPane>
+                </Tabs>
                 <Footer />
             </div>
         );
