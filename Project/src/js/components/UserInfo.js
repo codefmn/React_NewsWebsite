d8566ae21e820f8a7d67450f1b1829cdc851c1e9
mfxy <mfxyzm@outlook.com>
2018-07-12 23:32:15 +1000

    Add User Bookmarks

--- Project/src/js/components/UserInfo.js
+++ Project/src/js/components/UserInfo.js
@@ -1,12 +1,63 @@
 import React from 'react';
-import {Button} from 'antd';
+import {Row, Col, Tabs, Card} from 'antd';
+import PCHeader from './PCHeader';
+import Footer from './Footer';
 
 const base_url = "http://newsapi.gugujiankong.com/Handler.ashx?action=";
 
 export default class UserInfo extends React.Component{
+    constructor(){
+        super();
+        this.state = {
+            bookmarks:[]
+        }
+    }
+
+    componentWillMount(){
+        var fetchOptions={
+            method: 'GET'
+        }
+        fetch(base_url+"getuc&userid="+localStorage.userID,fetchOptions)
+            .then(response=>response.json())
+            .then(json=>{
+                this.setState({bookmarks:json});
+            });
+    }
+
     render(){
+        // const bookmarksList = this.state.bookmarks.length ?
+        //     this.state.bookmarks.map((bookmark)=>{
+        //         <Card key={bookmark.uniquekey} title={bookmark.uniquekey} 
+        //             extra={<a href={`/details/${bookmark.uniquekey}`}>View</a>}>
+        //             <p>{bookmark.Title}</p>
+        //         </Card>
+        //         })
+        //     :"No bookmarks yet.";
+
         return(
-            <div>test</div>
+            <div>
+                <PCHeader/>
+                <Row>
+                    <Col span={2}></Col>
+                    <Col span={20}>
+                        <Tabs>
+                            <Tabs.TabPane key="1" tab="My Bookmarks">
+                            Content:{
+                                this.state.bookmarks.map((bookmark)=>{
+                                    <Card key={bookmark.uniquekey} title={bookmark.uniquekey} 
+                                        extra={<a href={`/details/${bookmark.uniquekey}`}>View</a>}>
+                                        {bookmark.Title}
+                                    </Card>})
+                            }
+                            </Tabs.TabPane>
+                            <Tabs.TabPane key="2" tab="My Comments"></Tabs.TabPane>
+                            <Tabs.TabPane key="3" tab="My Avatar"></Tabs.TabPane>
+                        </Tabs>
+                    </Col>
+                    <Col span={2}></Col>
+                </Row>
+                <Footer/>
+            </div>
         );
     }
 }
